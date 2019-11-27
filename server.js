8a6f4f1497cc66285705146d23f986cbf42fa3e7
Aaron Miller <aaron.miller@mapbox.com>
2019-11-27 09:26:05 -0800

    mvp

--- server.js
+++ server.js
@@ -2,7 +2,13 @@
 projectData = {};
 
 // Require Express to run server and routes
+/* Express to run server and routes */
+const express = require('express');
+/* Start up an instance of app */
+const app = express();
 
+/* Dependencies */
+const bodyParser = require('body-parser')
 // Start up an instance of app
 
 /* Middleware*/
@@ -11,9 +17,56 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
-
+const cors = require('cors');
+app.use(cors());
 // Initialize the main project folder
 app.use(express.static('website'));
 
 
 // Setup Server
+const openWeatherAPIKEY = 'c2da5b47cc5e697eff5e9c844de10474';
+
+///from excercise
+/* Empty JS object to act as endpoint for all routes */
+projectData = {};
+
+
+
+
+/* Initialize the main project folder*/
+app.use(express.static('website'));
+
+const port = 3000;
+/* Spin up the server*/
+const server = app.listen(port, listening);
+function listening() {
+  // console.log(server);
+  console.log(`running on localhost: ${port}`);
+};
+
+// TODO-ROUTES!
+
+//GET route;
+app.get('/all', sendData);
+
+function sendData(request, response) {
+  response.send(data);
+};
+
+// POST route
+app.post('/add', callBack);
+
+function callBack(req, res) {
+  res.send('POST received');
+}
+
+// POST an animal
+const data = [];
+
+app.post('/addWeatherData', addWeatherData);
+
+function addWeatherData(req, res) {
+  data.push(req.body);
+  console.log(data);
+
+};
--- website/app.js
+++ website/app.js
@@ -1,5 +1,106 @@
 /* Global Variables */
+// Personal API Key for OpenWeatherMap API
+const apiKey = ',us&appid=c2da5b47cc5e697eff5e9c844de10474'
+///api.openweathermap.org/data/2.5/weather?zip={zip code},{country code} &appid=b6907d289e10d714a6e88b30761fae22
+const baseURL = 'http://api.openweathermap.org/data/2.5/weather?zip='
+//local node url for adding projectData
+const addDataURL = '/addWeatherData'
+const projDataURL = '/all'
 
-// Create a new date instance dynamically with JS
 let d = new Date();
-let newDate = d.getMonth()+'.'+ d.getDate()+'.'+ d.getFullYear();
+let newDate = d.getMonth() + '.' + d.getDate() + '.' + d.getFullYear();
+//temp values for latest reading
+let weatherData = {};
+let mostRecentRecord = [];
+
+/* Function called by event listener */
+
+document.getElementById('generate').addEventListener('click', callOpenWeather);
+
+
+
+function postGet(APItemp) {
+    ;
+    content = document.getElementById('feelings').value;
+    postData(addDataURL, { temperature: APItemp, date: newDate, userResponse: content });
+
+
+    getProjData('/all')
+        .then(function (data) {
+
+            let recID = (data.length - 1);
+            mostRecentRecord = data[recID];
+
+            document.getElementById('date').innerHTML = 'Date: ' + mostRecentRecord.date;
+            document.getElementById('temp').innerHTML = 'Temperature: ' + mostRecentRecord.temperature;
+            document.getElementById('content').innerHTML = 'Feelings: ' + mostRecentRecord.userResponse;
+
+        })
+
+}
+
+//function to make API call for openWeather data
+function callOpenWeather(e) {
+
+    getWeatherData(baseURL, document.getElementById('zip').value, apiKey)
+        .then(function (openWeatherData) {
+            postGet(openWeatherData.main.temp);
+        })
+}
+
+/* Function to GET OpenWeather Data*/
+const getWeatherData = async (baseURL, zip, key) => {
+    const response = await fetch(baseURL + zip + key);
+
+    try {
+        const openWeatherData = await response.json();
+        weatherData = openWeatherData;
+        return openWeatherData
+    } catch (error) {
+
+        console.log("error", error);
+    }
+}
+
+
+
+/* Function to GET Project Data */
+const getProjData = async (url = '') => {
+    const request = await fetch(url);
+    try {
+        // Transform into JSON
+
+        const allData = await request.json()
+        return allData;
+        console.log(allData);
+    }
+    catch (error) {
+        console.log("error", error);
+        //  handle the error
+    }
+};
+
+
+/* Function to POST data */
+
+const postData = async (url = '', data = {}) => {
+
+    const response = await fetch(url, {
+        method: 'POST',
+        credentials: 'same-origin', // include, *same-origin
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data),
+    });
+
+    try {
+        const newData = await response.json();
+        return newData
+    } catch (error) {
+        console.log("error", error);
+        //  handle the error
+    }
+};
+
+
